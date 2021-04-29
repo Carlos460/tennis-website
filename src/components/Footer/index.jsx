43527f0ac4bf8d53b7e50957e6354cc705fd307d
Carlos Flores <carlosfloresalex@gmail.com>
2021-04-28 20:15:36 -0400

    Added footer layout

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
+import * as S from './styles';
+
 export default function Footer() {
   return (
     <>
-      <h1>Footer Here</h1>
+      <S.FooterContainer>
+        <S.LinkContainer>
+          <S.LinkCol>
+            <S.Link>Link 1</S.Link>
+            <S.Link>Link 2</S.Link>
+            <S.Link>Link 3</S.Link>
+          </S.LinkCol>
+          <S.LinkCol>
+            <S.Link>Link 1</S.Link>
+            <S.Link>Link 2</S.Link>
+          </S.LinkCol>
+          <S.LinkCol>
+            <S.Link>Link 1</S.Link>
+            <S.Link>Link 2</S.Link>
+            <S.Link>Link 3</S.Link>
+          </S.LinkCol>
+          <S.LinkCol>
+            <S.Link>Link 1</S.Link>
+            <S.Link>Link 2</S.Link>
+            <S.Link>Link 3</S.Link>
+            <S.Link>Link 4</S.Link>
+          </S.LinkCol>
+        </S.LinkContainer>
+      </S.FooterContainer>
     </>
   );
 }
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.div`
+  width: 100%;
+  height: 300px;
+  display: flex;
+`;
+
+export const LinkContainer = styled.div`
+  margin: 0px auto;
+  width: 1000px;
+  display: flex;
+  justify-content: space-around;
+  border-bottom: 1px solid black;
+`;
+
+export const LinkCol = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Link = styled.a`
+  color: black;
+`;
+
+// export const LogoLinkContainer = styled.div``;
