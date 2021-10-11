caffad3a59c4ffdf70a4c337fdf4e61089917fa8
Carlos Flores <carlosfloresalex@gmail.com>
2021-10-11 16:25:52 -0400

    Updated UI for login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -5,12 +5,36 @@ import * as S from './styles';
 
 export default function LoginPage() {
   return (
-    <S.PageContainer>
-      <S.Form action="/">
-        <S.UserInput placeholder="Username" />
-        <S.PasswordInput placeholder="Password" />
-        <S.SubmitButton type="submit" value="Submit" />
-      </S.Form>
-    </S.PageContainer>
+    <S.Header>
+      <S.HeaderCol>
+        <S.Text>Join Us Today and Explore!</S.Text>
+      </S.HeaderCol>
+      <S.HeaderCol>
+        <S.Form action="/">
+          <S.FormRow>
+            <S.FormCol>
+              <S.Input placeholder="First Name" />
+            </S.FormCol>
+            <S.FormCol>
+              <S.Input placeholder="Last Name" />
+            </S.FormCol>
+          </S.FormRow>
+          <S.FormRow>
+            <S.FormCol>
+              <S.Input placeholder="Email" />
+            </S.FormCol>
+          </S.FormRow>
+          <S.FormRow>
+            <S.FormCol>
+              <S.Input placeholder="Password" />
+            </S.FormCol>
+          </S.FormRow>
+          <S.FormRow></S.FormRow>
+          <S.FormRow>
+            <S.SubmitButton type="submit" value="Submit" />
+          </S.FormRow>
+        </S.Form>
+      </S.HeaderCol>
+    </S.Header>
   );
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,31 +1,81 @@
 import styled from 'styled-components';
 
-export const PageContainer = styled.div`
-  padding: 75px 0px;
+export const Header = styled.div`
+  padding: 75px 10px;
+  width: 100%;
   min-height: 100vh;
+
+  display: flex;
+  justify-content: space-around;
+
+  @media (max-width: 1000px){
+    flex-direction: column;
+  }
 `;
 
-export const Input = styled.input`
-  width: 200px;
-  height: 30px;
+export const HeaderCol = styled.div`
+  display: flex;
+  width: 50%;
+  @media (max-width: 1000px){
+    width: 100%;
+  }
+`;
 
-  margin: 15px auto;
-  padding: 0px 2px;
+export const Text = styled.p`
+  max-width: 800px;
+  height: 400px;
+  display: flex;
+  justify-content: center;
+  margin: auto;
+  
+  text-align: center;
+  font-size: 84px;
+  font-weight: 700;
 `;
 
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
-  width: 100%;
+  width: 500px;
+  height: min-content;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+
+  margin: auto;
+  padding: 30px 0px 15px;
 `;
 
-export const UserInput = styled(Input)``;
+export const FormRow = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 20px;
+`;
 
-export const PasswordInput = styled(Input)``;
+export const FormCol = styled.div`
+  width: 100%;
+  margin: 0px 15px;
+`;
 
-export const SubmitButton = styled.input`
-  width: 100px;
+export const Input = styled.input`
+  width: 100%;
   height: 30px;
 
-  margin: 5px auto;
+  padding: 0px 2px;
+`;
+
+export const SubmitButton = styled.input`
+  color: white;
+  background-color: #ffae2e;
+  border: none;
+  border-radius: 2px;
+  cursor: pointer;
+
+  font-weight: 600;
+  font-size: 18px;
+
+  width: 50%;
+  height: 50px;
+  transition: background-color .2s ease;
+  &:hover {
+    background-color: #ffb94a;  
+  }
 `;
